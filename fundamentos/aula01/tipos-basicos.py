296df6313f7c3ddd6c98cb4be4a34ec5bcd7c1bc
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2025-01-29 21:05:11 -0300

    Cria Aula1 sobre Tipos Básicos e adiciona ao Fundamentos.ipynb

---
+++ fundamentos/aula01/tipos-basicos.py
@@ -0,0 +1,29 @@
+# Tipos Básicos
+
+# Imprime o valor booleano "True" (verdadeiro) na tela.
+print(True)  
+
+# Imprime o valor booleano "False" (falso) na tela.
+print(False)  
+
+# Realiza uma operação de soma (1.2 + 1) e imprime o resultado, que é 2.2.
+print(1.2 + 1)  
+
+# Imprime a string 'Esse é meu idioma' na tela.
+print('Esse é meu idioma')  
+
+# Imprime a string "Com aspas duplas também funciona" na tela.
+print("Com aspas duplas também funciona")  
+
+# Concatena várias strings: 'Você é ' + ('muito' * 2) + 'dahora!' 
+# (onde 'muito' é repetido duas vezes) e imprime o resultado: 'Você é muitomuitodahora!'.
+print('Você é ' + 2 * 'muito' + 'dahora!')  
+
+# Imprime a lista [1, 2, 3] na tela.
+print([1, 2, 3])  
+
+# Imprime o dicionário {'nome': 'Davi', 'sobrenome': 'Tanaka'} na tela.
+print({'nome': 'Davi', 'sobrenome': 'Tanaka'})  
+
+# Imprime "None", que representa a ausência de valor ou um valor nulo em Python.
+print(None)  
