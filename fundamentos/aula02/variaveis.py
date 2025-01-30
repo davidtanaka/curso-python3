743b82e97adbc5d938423ea403bbec29cfdef833
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2025-01-29 21:41:15 -0300

    Criando Aula2 sobre Variáveis e adiciona ao Fundamentos.ipynb

---
+++ fundamentos/aula02/variaveis.py
@@ -0,0 +1,10 @@
+# Variáveis: Definição de variáveis e operações com elas
+
+a = 10  # A variável 'a' é inicializada com o valor inteiro 10.
+b = 5.2  # A variável 'b' é inicializada com o valor decimal (float) 5.2.
+
+print(a + b)  # Imprime o resultado da soma entre 'a' e 'b'. O valor será 15.2, pois 10 (inteiro) + 5.2 (float) = 15.2.
+
+a = 'Agora eu sou uma string!'  # A variável 'a' é reatribuída com uma string. Agora 'a' não é mais um número, mas uma string.
+print(a)  # Imprime o conteúdo da variável 'a', que agora é a string 'Agora eu sou uma string!'.
+print(b)  # Imprime o valor da variável 'b', que continua sendo o número 5.2 (não foi alterado).
