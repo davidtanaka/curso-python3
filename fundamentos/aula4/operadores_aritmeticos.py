0a1c0b59ddc44877488a537ff346ef25d93c816b
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2025-01-31 23:58:37 -0300

    Criando Aula4 sobre Operadores Aritméticos e adiciona em Fundamentos.ipynb

---
+++ fundamentos/aula4/operadores_aritmeticos.py
@@ -0,0 +1,11 @@
+print(4 + 3) # Operador de soma
+print(2 - 1) # Operador de subtração
+print(2 * 6.3) # Operador de multiplicação
+print(8 / 4.2) # Operador de divisão
+print(8 // 4.2) # operador de divisão (mas vai retornar um inteiro.)
+print(5 ** 3) # Operador de Exponenciação
+print(9 % 5) # Esse operador reprsenta o resto da divisão
+
+a = 12
+b = a
+print(a + b)
