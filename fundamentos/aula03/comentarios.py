72edc263fa21135922d08f2c0993ebd0fd9e7961
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2025-01-31 23:44:04 -0300

    Criando Aula3 sobre comentários e adiciona em Fundamentos.ipynb

---
+++ fundamentos/aula03/comentarios.py
@@ -0,0 +1,20 @@
+""" 
+Comentários são muito importantes, mas devem ser usados com sabedoria.
+É melhor ter um código autoexplicativo do que um código que, para ser entendido, precise de diversos comentários.
+"""
+
+# Minhas variáveis
+salario = 1.412
+despesas = 1.236
+
+
+"""
+A ideia é calcular o
+quanto vai sobrar do
+salário no fim do mês.
+"""
+sobra = salario - despesas
+print(sobra)
+
+# print(fim)
+print('Fim de verdade') # comentário também funciona aqui
