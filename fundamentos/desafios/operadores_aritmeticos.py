2f212b6a3af599ca39a3d0e1f006a2198025e14e
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2025-02-01 21:13:48 -0300

    Desafio de operadores aritméticos, já com a solução do prof.

---
+++ fundamentos/desafios/operadores_aritmeticos.py
@@ -0,0 +1,12 @@
+# O desafio consiste em verificar qual porcentagem do salário 
+# será consumida pelas despesas.
+
+salario = 1.412
+despesas = 1.236
+
+porcentagem_de_gastos = (despesas / salario) * 100
+print(f'Suas despesas vai utilizar um total de -> {porcentagem_de_gastos:.0f}% do valor de seu salário')
+
+# Solução do professor
+percentual_comprometido = despesas / salario * 100
+print(percentual_comprometido)
