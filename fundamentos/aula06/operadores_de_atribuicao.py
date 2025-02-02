a8a17960876ebb37e295f6ff72704dd94b794015
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2025-02-01 21:45:12 -0300

    Criando Aula6 sobre Operadores de Atribuição e adicionando em Fundamentos.ipynb

---
+++ fundamentos/aula06/operadores_de_atribuicao.py
@@ -0,0 +1,27 @@
+# Este código demonstra o uso de operadores de atribuição em Python.
+# Os operadores de atribuição permitem atribuir valores a variáveis e realizar operações de atribuição combinadas.
+
+a = 4  # Atribui o valor 4 à variável a
+a = a + 7  # Adiciona 7 ao valor atual de a e atribui o resultado a a
+print(a)  # Imprime o valor atualizado de a
+
+a += 5  # Equivalente a a = a + 5
+print(a)
+
+a -= 2  # Equivalente a a = a - 2
+print(a)
+
+a *= 3  # Equivalente a a = a * 3
+print(a)
+
+a /= 1  # Equivalente a a = a / 1
+print(a)
+
+a %= 4  # Equivalente a a = a % 4 (resto da divisão por 4)
+print(a)
+
+a **= 6  # Equivalente a a = a ** 6 (elevação de a à potência 6)
+print(a)
+
+a //= 453  # Equivalente a a = a // 453 (divisão inteira por 453)
+print(a)
