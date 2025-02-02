e1019322c32648590f1f7e6f6fad97ed50002022
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2025-02-01 21:26:56 -0300

    Criando Aula5 sobre Operadores Relacionais e adicionando em Fundamentos.ipynb

---
+++ fundamentos/aula05/operadores_relacionais.py
@@ -0,0 +1,9 @@
+# Exemplos de operadores relacionais: >, >=, <, <=, !=, ==
+# Comparações entre números inteiros e strings
+print(3 > 4)  # Maior que
+print(4 >= 3) # Maior ou igual
+print(1 < 2)  # Menor que
+print(3 <= 1) # Menor ou igual
+print(3 != 2) # Diferente
+print(4==4)  # Igual
+print(4=='4') # Igualdade entre tipos diferentes
