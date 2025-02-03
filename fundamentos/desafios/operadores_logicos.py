533f0e914d296927f187299e42cc3a7612b2c0ef
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2025-02-03 16:01:04 -0300

    Desafio sobre operadores lógicos (solução do professor)

--- fundamentos/desafios/operadores_logicos.py
+++ fundamentos/desafios/operadores_logicos.py
@@ -19,3 +19,15 @@ saude = not sorvete
 
 print(f'Tv de 50 polegadas {tv_50}, Tv de 32 polegadas {tv_32}')
 print(f'Você tomou sorvete? {sorvete}, sua Saúde esta boa? {saude}')
+
+# Solução do professor
+trabalho_terca = False
+trabalho_quinta = False
+
+tv_50 = trabalho_terca and trabalho_quinta
+sorvete = trabalho_terca or trabalho_quinta
+tv_32 = trabalho_terca != trabalho_quinta #xor
+mais_saudavel = not sorvete
+
+print("Tv50={} Tv32={} Sorvete={} Saudável={}"
+     .format(tv_50, tv_32, sorvete, mais_saudavel))
