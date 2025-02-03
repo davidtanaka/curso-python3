5d4c777b49831484baa272410013cea1e774d027
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2025-02-03 16:31:48 -0300

    Criando Aula8 sobre Operadores Unários e adicionando em Fundamentos.ipynb

---
+++ fundamentos/aula08/operadores_unarios.py
@@ -0,0 +1,12 @@
+# Operadores unários
+
+a = 3  # Atribui o valor 3 à variável 'a'
+
+# a++  # Incrementaria 'a' em 1, mas está comentado
+a += 1  # Incrementa 'a' em 1 (equivalente a a = a + 1)
+
+# a--  # Decrementaria 'a' em 1, mas está comentado
+a -= 1  # Decrementa 'a' em 1 (equivalente a a = a - 1)
+
+a = -a  # Inverte o sinal de 'a' (se era positivo, fica negativo e vice-versa)
+print(a)
