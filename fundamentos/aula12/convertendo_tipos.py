8c239bfbaab7645908adce532c8e86a59adbdada
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2025-02-04 16:40:32 -0300

    Aula 12 sobre Conversão de Tipos adicionando em fundamentos.ipynb

---
+++ fundamentos/aula12/convertendo_tipos.py
@@ -0,0 +1,12 @@
+print(8 + 1)  # Soma entre números inteiros (8 + 1 = 9)
+
+print('8' + '1')  # Concatenação de strings ('8' + '1' = '81')
+
+a = 8
+b = '4'  # b é uma string
+
+print(a + int(b))  # Converte b para inteiro e soma (8 + 4 = 12)
+
+print(str(a) + b)  # Converte a para string e concatena ('8' + '4' = '84')
+
+print(a + float(5.6))  # Converte 5.6 para float e soma (8 + 5.6 = 13.6)
