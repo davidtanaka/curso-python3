f9a9d276f447d2a97700921dc48ac2b43d3aab31
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2025-02-04 16:32:43 -0300

    Aula 11 sobre Builtins e adicionando em fundamentos.ipynb

--- fundamentos/aula10/outros_operadores.py
+++ fundamentos/aula10/outros_operadores.py
@@ -1,18 +1,18 @@
 # Operadores de Membro
 lista = [1, 2, 3, 'Ana', 'Claudia']
-print(2 in lista)  # Verifica se o elemento 2 está na lista (retorna True)
+print(2 in lista)
 
 # Operadores de Identidade
 x = 4
-y = x  # y recebe a mesma referência de x
-z = 4  # Python pode reutilizar a mesma referência para pequenos inteiros
-print(x is y)  # True, pois y é exatamente a mesma variável que x
-print(y is z)  # True, pois z também pode compartilhar a mesma referência
-print(x is not z)  # False, pois x e z são a mesma referência
+y = x
+z = 4
+print(x is y)
+print(y is z)
+print(x is not z)
 
 lista1 = [9, 8, 4]
-lista2 = lista  # lista2 recebe a mesma referência da lista original
-lista3 = [9, 8, 4]  # Criada uma nova lista, mesmo conteúdo, mas referência diferente
-print(lista1 is lista2)  # False, pois lista1 e lista2 são objetos diferentes
-print(lista1 is lista3)  # False, pois lista1 e lista3 são listas distintas
-print(lista1 is not lista3)  # True, pois lista1 e lista3 têm referências diferentes
+lista2 = lista
+lista3 = [9, 8, 4]
+print(lista1 is lista2)
+print(lista1 is lista3)
+print(lista1 is not lista3)
---
+++ fundamentos/aula11/builtins.py
@@ -0,0 +1,9 @@
+# type()
+type(1)
+
+__builtins__.type('Olá, mundo!')
+__builtins__.print(10 / 3)
+
+# __builtins__.help(__builtins__.dir)
+
+dir(__builtins__)
