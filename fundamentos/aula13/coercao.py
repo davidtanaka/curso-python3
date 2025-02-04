096fd418167ee6966270a16e18ccd21ed5bbcabe
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2025-02-04 16:47:56 -0300

    Aula 13 sobre Coerção Automática adicionando em fundamentos.ipynb

---
+++ fundamentos/aula13/coercao.py
@@ -0,0 +1,20 @@
+print(10 / 2)  # Divisão normal (10 / 2 = 5.0)
+print(type(10 / 2))  # Mostra o tipo do resultado (float)
+
+print(10 / 3)  # Divisão normal (10 / 3 = 3.3333...)
+
+print(10 // 3)  # Divisão inteira (10 // 3 = 3)
+print(type(10 // 3))  # Mostra o tipo do resultado (int)
+
+print(10 // 3.3)  # Divisão inteira com float (10 // 3.3 = 3.0)
+print(type(10 // 3.3))  # Mostra o tipo do resultado (float)
+
+print(10 / 2.5)  # Divisão normal (10 / 2.5 = 4.0)
+
+print(2 + True)  # True é tratado como 1, então (2 + 1 = 3)
+
+print(2 + False)  # False é tratado como 0, então (2 + 0 = 2)
+
+print(type(1 + 2))  # Soma de inteiros retorna um inteiro (int)
+
+print(type(1 + 2.5))  # Soma de inteiro com float retorna float (float)
