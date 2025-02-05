0cdbb9e37501d049f1a31afaaa113c423b372256
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2025-02-05 16:57:30 -0300

    Aula 2 sobre strings (fundamentos)

---
+++ fundamentos/strings/aula2.py
@@ -0,0 +1,14 @@
+nome = 'Ana Paula'
+print(nome[0])
+print(nome[6])
+print(nome[-3])
+print(nome[:4])
+print(nome[:-5])
+print(nome[:-5])
+print(nome[:3])
+print(nome[::-1])
+
+
+numeros = '123456789'
+print(numeros[::-1])
+print(numeros[1::2])
