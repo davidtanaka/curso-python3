5906d1adc4fdb2f85996790732a1ce038ebe9189
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2025-02-06 17:26:53 -0300

    Aula 3 sobre Listas

---
+++ fundamentos/listas/aula3.py
@@ -0,0 +1,13 @@
+lista = ['João', 'Vitor', 'Henrique', 'Fabricio', 'Daniel']
+
+print(lista[1:4])
+print(lista[0:3])
+print(lista[::2])
+print(lista[::-1])
+
+del lista[3]
+print(lista)
+del lista[3]
+print(lista)
+del lista[1:]
+print(lista)
