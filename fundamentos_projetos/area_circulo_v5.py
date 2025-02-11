e85d03fcdfb11e8c6d7ecfc8da696c2460466b41
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2025-02-11 19:50:37 -0300

    Obtendo dados do usuário (input)

---
+++ fundamentos_projetos/area_circulo_v5.py
@@ -0,0 +1,9 @@
+from math import pi
+ 
+# print('pi = ', math.pi)
+
+area_pi = pi
+raio = float(input('Informe o raio: '))
+resultado = area_pi * raio ** 2
+
+print(f'Área do círculo é igual à: {resultado:.2f}')
