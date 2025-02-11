11463510f208f3a074b17b6e0b5e8ff5ca81d6da
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2025-02-11 16:18:43 -0300

    Encoding

---
+++ fundamentos_projetos/area_circulo_v2.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+area_pi = 3.1415
+raio = 8
+resultado = area_pi * raio ** 2
+
+print(f'O valor de {area_pi} * {raio}² é igual á {resultado}')
---
+++ fundamentos_projetos/enconding_teste.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# print "העלא וועלט"
+# print "Olá Mundo!"
