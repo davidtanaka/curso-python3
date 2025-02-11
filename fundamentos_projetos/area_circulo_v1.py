2ef03d270e253baa2c2d57cca4baeea49854556b
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2025-02-11 16:01:37 -0300

    Desafio 1, minha solução e do professor

---
+++ fundamentos_projetos/area_circulo_v1.py
@@ -0,0 +1,11 @@
+# Minha solução do desafio
+area_pi = 3.1415
+raio = 8
+resultado = area_pi * raio ** 2
+
+print(f'O valor de {area_pi} * {raio}² é igual á {resultado}')
+
+# Versão do professor
+pi = 3.14159
+raio_ = 15.3
+print('Área do círculo', pi * raio ** 2)
