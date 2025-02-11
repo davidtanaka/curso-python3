3114b866aa0d2278e48425d707e6bdf8cd317314
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2025-02-11 20:03:31 -0300

    Um Pouco Sobre Módulo

---
+++ fundamentos_projetos/area_circulo_v6.py
@@ -0,0 +1,9 @@
+from math import pi
+
+area_pi = pi
+raio = float(input('Informe o raio: '))
+resultado = area_pi * raio ** 2
+
+print(f'Área do círculo é igual à: {resultado:.2f}')
+
+print('Nome do módulo', __name__)
