64fd3b09619336c80ad144641982a1590b9b5e45
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2025-02-12 15:19:27 -0300

    Testando Se é o Módulo Principal

---
+++ fundamentos_projetos/area_circulo_v7.py
@@ -0,0 +1,9 @@
+from math import pi
+
+if __name__ =='__main__':
+    area_pi = pi    
+    raio = float(input('Informe o raio: '))
+    resultado = area_pi * raio ** 2
+
+    print(f'Área do círculo é igual à: {resultado:.2f}')
+
