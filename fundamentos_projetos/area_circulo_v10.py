40e7d33c6c48804cbe6a8df6190149400cf0e891
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2025-02-12 16:30:52 -0300

    Obtendo Argumento Via Terminal

---
+++ fundamentos_projetos/area_circulo_v10.py
@@ -0,0 +1,13 @@
+from math import pi
+import sys
+
+
+def circulo():
+    area_pi = pi
+    raio = int(sys.argv[1])
+    resultado = area_pi * raio ** 2
+    return resultado
+
+
+if __name__ =='__main__':
+    print('A área do circulo igual é: ', circulo())
