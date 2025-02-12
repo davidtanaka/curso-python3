962065d515b0d0457ae65157f775c0c4cc16c138
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2025-02-12 16:40:58 -0300

    Validando Existência do Argumento

--- fundamentos_projetos/area_circulo_v10.py
+++ fundamentos_projetos/area_circulo_v10.py
@@ -4,9 +4,12 @@ import sys
 
 def circulo():
     area_pi = pi
-    raio = int(sys.argv[1])
-    resultado = area_pi * raio ** 2
-    return resultado
+    if len(sys.argv) < 2:
+        print('É necessário informar o raio,\n Sintaxe: area_ciraculo <raio>')
+    else:
+        raio = int(sys.argv[1])
+        resultado = area_pi * raio ** 2
+        return resultado
 
 
 if __name__ =='__main__':
