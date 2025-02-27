a229dbe7cdc6dd7ae9bd8af0a6ec40931e495613
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2025-02-27 16:05:13 -0300

    Fazendo o código sem list comprehension

--- list_comprehension/comprehension_v1.py
+++ list_comprehension/comprehension_v1.py
@@ -1,3 +1,9 @@
 # [ exepressão for item in list if condicional ]
 dobros = [i * 2 for i in range(10)]
 print(dobros)
+
+# Sem list comprehension
+dobros = []
+for i in range(10):
+    dobros.append(i * 2)
+print(dobros)
