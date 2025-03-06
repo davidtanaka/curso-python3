647985a66000a62b5fa281a309303c14577dbd02
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2025-03-06 18:30:00 -0300

    129. Callable com (Un)Packing Nomeado

---
+++ funcoes/callable_packing_nomeado.py
@@ -0,0 +1,22 @@
+def calcula_preco(preco_bruto, calc_imposto, **params):
+    return preco_bruto + preco_bruto * calc_imposto(**params)
+
+
+def imposto_x(importado):
+    return 0.22 if importado else 0.13
+
+
+def imposto_y(explosivo, nivel_perigo=1):
+    return 0.11 * nivel_perigo if explosivo else 0
+
+
+if __name__ == '__main__':
+    # fone = calcula_preco(20, imposto_x, True)
+    # bomba = calcula_preco(200, imposto_y, True, 5)
+    # print(bomba)
+
+    preco_bruto = 5000
+    preco_final_foguete = calcula_preco(preco_bruto, imposto_x, importado=True)
+    preco_final_foguete = calcula_preco(preco_final_foguete, imposto_y,
+                                         explosivo=True, nivel_perigo=5)
+    print(f'Preço final: R$ {preco_final_foguete}')
