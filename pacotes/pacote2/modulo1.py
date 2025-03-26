f7765323b38e06f62d254c07e50c10560210636d
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2025-03-26 16:27:04 -0300

    Módulos com mesmo nome

---
+++ pacotes/pacote2/modulo1.py
@@ -0,0 +1,6 @@
+print(f'estou dentro do módulo => {__name__} do pacote => {__package__}')
+
+
+def subtracao(a, b):
+    return a - b
+
---
+++ pacotes/pacote_v3.py
@@ -0,0 +1,6 @@
+from pacote1 import modulo1
+from pacote2 import modulo1 as modulo1_sub
+
+
+print('Soma:', modulo1.soma(6, 2))
+print('Subtração:', modulo1_sub.subtracao(10, 2))
